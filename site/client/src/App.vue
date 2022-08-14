370625bf1e7474b1825f1945651fb2806789ad3e
amir <amir.kashapov@protonmail.ch>
2022-08-14 23:34:37 +1000

    feat: added test to fetch data

--- site/client/src/App.vue
+++ site/client/src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="fetchData">Fetch Data</button>
+  <p>Data retrieved: {{ message }}</p>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+const axios = require('axios');
 
+const message = ref("");
+
+function fetchData(){
+  axios.get("/start_python").then(function (response){
+    console.log(response);
+    message.value = response;
+  });
+}
+</script>
+
+<script>
 export default {
   name: 'App',
   components: {
